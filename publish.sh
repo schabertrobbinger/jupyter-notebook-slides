bbe4d9183fec9e81e5d5903813edf7f99c44ea8f
enigma <schabertrobbinger@gmail.com>
2020-11-19 03:04:32 -0500

    Update

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 jupyter-nbconvert --to slides Presentation.ipynb --reveal-prefix=reveal.js
-mv slides.slides.html  index.html
+mv Presentation.slides.html  index.html
 mkdir -p /tmp/workspace
 cp -r * /tmp/workspace/
 git add -A .
